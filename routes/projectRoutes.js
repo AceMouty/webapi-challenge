959da9e903094ccaf0234edf12a472b880aee541
AceMouty <kyleguerrero85@gmail.com>
2019-10-04 10:51:37 -0500

    api now supports post requests to the projects schema

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -10,6 +10,14 @@ router.get("/", (req, res) => {
 })
 
 // CREATE: Post a new project
+router.post("/", (req, res) => {
+    const createProject = req.body
+    Projects.insert(createProject)
+    .then(newProject => {
+        res.status(200).json({data: newProject})
+    })
+    .catch(err => res.status(500).json({message: "There was a problem in the server", err:err}))
+})
 
 
 module.exports = router;
